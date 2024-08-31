a5af12aa4b3f1a79612d6de737f5942b9c6d947e
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-31 20:18:03 +0530

    update: add validations for task input, handle error

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+const { z } = require("zod");
+
 const Task = require("../models/taskModel");
 const taskRouter = express.Router();
 
+const taskSchema = z.object({
+  title: z.string(),
+  summary: z.string(),
+  assignee: z.string(),
+  priority: z.string(),
+});
+
 taskRouter.post("/", async (req, res) => {
   try {
-    const task = await Task.create({
-      id: uuidv4(),
-      title: "Task 3",
-      summary: "Summary for Task 3",
-      assignee: "Pete",
-      date: new Date(),
-      priority: "High",
-    });
-    if (task) {
-      res.status(201).json({ messgae: "task created successfully" });
+    const result = taskSchema.safeParse(req.body);
+    if (result.success) {
+      const task = await Task.create({
+        id: uuidv4(),
+        date: new Date(),
+        ...req.body,
+      });
+      if (task) {
+        res.status(201).json({ message: "task created successfully" });
+      } else {
+        res.status(400).json({ message: "Failed to add task" });
+      }
     } else {
-      res.status(401).json({ message: "some error occurred" });
+      res.status(400).json({ message: "Invalid Input" });
     }
-  } catch (err) {
-    res.status(500).json({ message: "some error occurred", err });
+  } catch (error) {
+    res.status(500).json({ message: "some error occurred", error });
   }
 });
 taskRouter.get("/", async (req, res) => {
-  const tasks = await Task.find({});
-  res.status(200).send(tasks);
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "some error occurred", error });
+  }
 });
 
 module.exports = taskRouter;
